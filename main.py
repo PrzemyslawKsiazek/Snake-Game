7742c95bd65519dcaac7237fbb4cc617c495462d
Vickul <pksiazek@protonmail.com>
2023-03-25 20:12:42 +0100

    snake game in turtle module

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,10 +23,15 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
+
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
